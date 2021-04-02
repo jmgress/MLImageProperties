49b1cccb903fb27a3cf80eb8784076d66c9566c4
James Gress <jamesgress@hotmail.com>
2021-04-02 16:59:22 -0400

    Add project files.

---
+++ MLImageProperties/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MLImageProperties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
