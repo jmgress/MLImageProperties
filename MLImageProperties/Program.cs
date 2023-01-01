a2af09ce48a7e9bc88e074fe29c319630486d82a
James Gress <jamesgress@hotmail.com>
2023-01-01 19:39:30 +0000

    Small Changes

--- MLImageProperties/Program.cs
+++ MLImageProperties/Program.cs
@@ -4,9 +4,8 @@ namespace MLImageProperties
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World! I am now writing code in GitHub Codespaces... Yea!!!");
-        }
+        private const string Value = "Hello World! I am now writing code in GitHub Codespaces... Yea!!!";
+
+        static void Main(string[] args) => Console.WriteLine(Value);
     }
 }
